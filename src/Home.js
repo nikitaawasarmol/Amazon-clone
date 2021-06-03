33653f5429a12a3bd0d983bf722bb7f281d1d868
nikitaawasarmol <nikitaawasarmol@gmail.com>
2021-06-03 22:07:21 +0530

    redesigned banner

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,68 @@ import Product from './Product';
 function Home() {
     return (
         <div className ="home">
-            <img className = "home__image" src="https://images-eu.ssl-images-amazon.com/images/G/31/gateway-2015/amazonshop/Desktop_Banner_Recruitment_Website.jpg"></img>
+            <img className = "home__image" src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Auto/PSS/Personal-Safety_1500x600._CB668022827_.jpg"></img>
         
         
         {/*products*/}
+        {/* 1st row */}
+        <div className = "home__row">
+
+        <Product 
+        id = "351243"
+        title = "Infinity (JBL) Glide 120 Metal in-Ear Wireless Flex Neckband with Bluetooth 5.0 and IPX5 Sweatproof (Black and Yellow)"
+        price = {"1,099"}
+        rating = {5}
+        image = "https://images-eu.ssl-images-amazon.com/images/I/31N9z2rXiBL._SX300_SY300_QL70_FMwebp_.jpg"
+        />
+
+        <Product   
+        id = "351243"
+        title = "ASUS TUF Gaming BP2700T 17-inch Gaming Laptop Backpack (Black), 1680D Water-Repellent Exterior, 18L Capacity & Tactical Camo Design, for Gaming and Everyday Use"
+        price = {"3,498.00"}
+        rating = {4}
+        image = "https://images-na.ssl-images-amazon.com/images/I/81BvxQbA0TL._SL1500_.jpg"
+        />
+
+        </div>
+
+        
+        
+         {/* 2nd row */}
+         <div className = "home__row">
+
+        <Product 
+        id = "351243"
+        title = " Fujifilm Instax Mini 9 Instant Camera (Smokey White) "
+        price = {"3,919.00"}
+        rating = {4}
+        image = "https://images-na.ssl-images-amazon.com/images/I/71EhsUOyLZL._SX425_.jpg"
+        />
+
+        <Product   
+        id = "351243"
+        title = "HP 22fw Ultra-Thin Full HD 21.5-inch IPS Monitor with VGA and HDMI Ports, AMD FreeSync(3KS61AA) "
+        price = {"9,999.00"}
+        rating = {5}
+        image = "https://images-na.ssl-images-amazon.com/images/I/71yYGgCG%2BhL._SL1500_.jpg"
+        />
+
+        <Product   
+        id = "351243"
+        title = "Infinity (JBL) Glide 120 Metal in-Ear Wireless Flex Neckband with Bluetooth 5.0 and IPX5 Sweatproof (Black and Yellow)"
+        price = {1099}
+        rating = {5}
+        image = "https://images-eu.ssl-images-amazon.com/images/I/31N9z2rXiBL._SX300_SY300_QL70_FMwebp_.jpg"
+        />
+
+        </div>
+
+
+       
+        {/* 3rd row */}
+
+        <div className = "home__row">
+
         <Product 
         id = "351243"
         title = "Infinity (JBL) Glide 120 Metal in-Ear Wireless Flex Neckband with Bluetooth 5.0 and IPX5 Sweatproof (Black and Yellow)"
@@ -16,6 +74,10 @@ function Home() {
         rating = {5}
         image = "https://images-eu.ssl-images-amazon.com/images/I/31N9z2rXiBL._SX300_SY300_QL70_FMwebp_.jpg"
         />
+     
+       
+       
+        </div>
         </div>
     )
 }
