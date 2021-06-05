ed6c650d22150d710d664505b1d4b7ca4434df40
nikitaawasarmol <nikitaawasarmol@gmail.com>
2021-06-05 19:22:14 +0530

    fetched context API

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,19 @@
 export const initialState = {
-    basket: ["Bread", "chicken"],
+    basket: [],
+    user: null,
 };
 
-function reducer(state, action) {
+const reducer = (state, action) => {
+    console.log(action);
     switch(action.type) {
         case 'ADD_TO_BASKET':
-            break;
+            return { 
+                ...state,
+                basket: [...state.basket, action.item],
+            };
         case 'REMOVE_FROM_BASKET':
-            break;
+            return { state } ; 
+       
         default:
             return state;
     }
